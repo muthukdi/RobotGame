c992f474e12be2f22c748c564591a1a63a63ca9c
Dilip Muthukrishnan <cheepu82@gmail.com>
2014-04-21 21:45:09 -0400

    Initial Commit

---
+++ RobotGame/Classes/AppDelegate.h
@@ -0,0 +1,13 @@
+//
+//  AppDelegate.h
+//  RobotGame
+//
+//  Created by Ed on 2014-04-21.
+//  Copyright Ed 2014. All rights reserved.
+//
+// -----------------------------------------------------------------------
+
+#import "cocos2d.h"
+
+@interface AppDelegate : CCAppDelegate
+@end
---
+++ RobotGame/Classes/HelloWorldScene.h
@@ -0,0 +1,27 @@
+//
+//  HelloWorldScene.h
+//  RobotGame
+//
+//  Created by Ed on 2014-04-21.
+//  Copyright Ed 2014. All rights reserved.
+//
+// -----------------------------------------------------------------------
+
+// Importing cocos2d.h and cocos2d-ui.h, will import anything you need to start using Cocos2D v3
+#import "cocos2d.h"
+#import "cocos2d-ui.h"
+
+// -----------------------------------------------------------------------
+
+/**
+ *  The main scene
+ */
+@interface HelloWorldScene : CCScene
+
+// -----------------------------------------------------------------------
+
++ (HelloWorldScene *)scene;
+- (id)init;
+
+// -----------------------------------------------------------------------
+@end
